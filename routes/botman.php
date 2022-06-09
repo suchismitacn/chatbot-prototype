4fd902dbafb6d7320dd9a0effd82d0786ae51c71
Suchismita B <suchismita.basu@capitalnumbers.com>
2022-06-09 15:37:45 +0530

    Botman issue with detecting dialogflow actions fixed

--- routes/botman.php
+++ routes/botman.php
@@ -6,15 +6,16 @@ use App\Conversations\ChatBotV2\OriginateConversation;
 use App\Middlewares\CapturedMiddleware;
 use App\Middlewares\HeardMiddleware;
 use App\Middlewares\SendingMiddleware;
-use BotMan\BotMan\BotMan;
+use BotMan\Middleware\DialogFlow\V2\DialogFlow;
 
 $botman = resolve('botman');
+
+$dialogflow = DialogFlow::create('en');
 
 // custom middlewares
 $botman->middleware->heard(new HeardMiddleware());
 $botman->middleware->captured(new CapturedMiddleware());
 $botman->middleware->sending(new SendingMiddleware());
-$dialogflow = \BotMan\Middleware\DialogFlow\V2\DialogFlow::create('en');
 $botman->middleware->received($dialogflow);
 
 // $botman->hears('start', function ($bot) {
@@ -37,8 +38,12 @@ $botman->hears('hi', function ($bot) {
     $bot->startConversation(new OriginateConversation);
 });
 
-$botman->hears('TestFAQ', function ($bot) {
+$botman->hears('testfaq.(.*)', function ($bot) {
     $extras = $bot->getMessage()->getExtras();
     \Log::debug("Extras: ".print_r($extras, true));
     $bot->reply($extras['apiReply']);
 })->middleware($dialogflow);
+
+$botman->fallback(function($bot) {
+    $bot->reply('Sorry, I did not understand that.');
+});
