5fd3c34341c97fc8e97afab30145166804a09bc0
Suchismita Basu <suchismita.basu@capitalnumbers.com>
2022-07-21 12:48:03 +0530

    Blade changes

--- resources/views/tinker.blade.php
+++ resources/views/tinker.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{config('app.name')}}</title>
+    <title>{{config('app.name')}} - Chatbot</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
